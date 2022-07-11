5f2f66b679b41649fd4c6339b749dec2ea622185
ceickmeyer <cody.eickmeyer@gmail.com>
2022-07-11 16:11:52 -0400

    Good job buddy

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,81 @@
+/* let js = 'amazing';
+console.log(40 + 8 + 32 - 10);
+
+let firstName = "Jonas";
+
+console.log(firstName)
+
+// variable name conventions
+let myFirstJob = "programmer";
+let myCurrentJob = 'teacher';
+
+
+true;
+
+let javascriptIsFun = 'fart the "dog"';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 13;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = null;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let age = 30;
+const birthYear = 1991;
+
+console.log(age)
+
+const year = 2037;
+const ageJonas = year - 1991;
+const ageSarah = year - 2018;
+
+console.log(ageJonas * 2, ageSarah / 2, ageSarah ** 3);
+
+const firstName = 'Cody';
+const lastName = 'Eickmeyer';
+
+console.log(firstName, lastName)
+
+let x = 10 + 5;
+
+x += 10;
+
+x++;
+
+console.log(x)
+
+//comparison operators
+
+console.log(ageJonas > ageSarah)
+
+
+const fart = 2037;
+const ageJonas = fart - 1991;
+const ageSarah = fart - 2018;
+
+console.log(fart - 1991 == fart - 2018);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(averageAge)
+ */
+
+const markMass = 78;
+const markHeight = 1.69;
+
+const johnMass = 92;
+const johnHeight = 1.95;
+
+let markBMI = markMass / markHeight ** 2
+console.log(markBMI)
+
+let johnBMI = johnMass / johnHeight ** 2
+console.log(johnBMI)
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI)
+
+17 - strings
